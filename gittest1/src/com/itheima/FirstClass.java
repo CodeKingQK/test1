384537534f6cefb7769889e1318b4f1cf6e916c0
qiankun <2579500585@qq.com>
2020-02-04 17:05:30 +0800

    111

--- gittest1/src/com/itheima/FirstClass.java
+++ gittest1/src/com/itheima/FirstClass.java
@@ -4,4 +4,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("123456");
     }
+    public void test(){
+        System.out.println("test");
+    }
 }
